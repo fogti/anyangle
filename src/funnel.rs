35d47ef8b134ae9aedc88d7eb0fc876a5e640f5e
Έλλεν Εμίλια Άννα Zscheile <fogti+devel@ytrizja.de>
2026-09-04 23:09:30 +0200

    fix(funnel): Don't carry epsilon everywhere, fix double apex yield

--- src/funnel.rs
+++ src/funnel.rs
@@ -3,93 +3,31 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 //! An implementation of the [simple stupid funnel algorithm](https://digestingduck.blogspot.com/2010/03/simple-stupid-funnel-algorithm.html).
 
-use core::{cmp::PartialOrd, fmt, mem};
-
 use approx::AbsDiffEq;
+use core::mem;
+use num_traits::Num;
 
 use crate::math::*;
 
-pub struct SimpleFunnel<K: AbsDiffEq, D = ()> {
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+pub struct SimpleFunnel<K, D = ()> {
     pub apex: ([K; 2], D),
     pub lhs: ([K; 2], D),
     pub rhs: ([K; 2], D),
-    pub epsilon: <K as AbsDiffEq>::Epsilon,
-}
-
-impl<K, D> Clone for SimpleFunnel<K, D>
-where
-    K: AbsDiffEq + Clone,
-    <K as AbsDiffEq>::Epsilon: Clone,
-    D: Clone,
-{
-    fn clone(&self) -> Self {
-        Self {
-            apex: self.apex.clone(),
-            lhs: self.lhs.clone(),
-            rhs: self.rhs.clone(),
-            epsilon: self.epsilon.clone(),
-        }
-    }
-
-    fn clone_from(&mut self, source: &Self) {
-        self.apex.clone_from(&source.apex);
-        self.lhs.clone_from(&source.lhs);
-        self.rhs.clone_from(&source.rhs);
-        self.epsilon.clone_from(&source.epsilon);
-    }
-}
-
-impl<K, D> Copy for SimpleFunnel<K, D>
-where
-    K: AbsDiffEq + Copy,
-    <K as AbsDiffEq>::Epsilon: Copy,
-    D: Copy,
-{
-}
-
-impl<K, D> fmt::Debug for SimpleFunnel<K, D>
-where
-    K: AbsDiffEq + fmt::Debug,
-    <K as AbsDiffEq>::Epsilon: fmt::Debug,
-    D: fmt::Debug,
-{
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("SimpleFunnel")
-            .field("apex", &self.apex)
-            .field("lhs", &self.lhs)
-            .field("rhs", &self.rhs)
-            .field("epsilon", &self.epsilon)
-            .finish()
-    }
-}
-
-impl<K, D> Default for SimpleFunnel<K, D>
-where
-    K: AbsDiffEq + Default,
-    D: Default,
-{
-    fn default() -> Self {
-        Self {
-            apex: Default::default(),
-            lhs: Default::default(),
-            rhs: Default::default(),
-            epsilon: <K as AbsDiffEq>::default_epsilon(),
-        }
-    }
 }
 
 impl<K, D> SimpleFunnel<K, D>
 where
-    K: AbsDiffEq + Clone + num_traits::Num + PartialOrd,
+    K: AbsDiffEq + Clone + Num + PartialOrd,
     <K as AbsDiffEq>::Epsilon: Clone,
     D: Clone,
 {
-    pub fn new(apex: ([K; 2], D), epsilon: <K as AbsDiffEq>::Epsilon) -> Self {
+    pub fn new(apex: ([K; 2], D)) -> Self {
         Self {
             apex: apex.clone(),
             lhs: apex.clone(),
             rhs: apex,
-            epsilon,
         }
     }
 
@@ -103,21 +41,19 @@ where
     /// edges of a _single_ triangulation (with faces being triangles) yields valid funnels,
     /// and similarly, using 2D polygonal complices (complexes) with convex faces and passing
     /// their edges into this method also works.
-    pub fn advance(&mut self, portal: [([K; 2], D); 2]) -> Option<(([K; 2], D), &([K; 2], D))> {
+    pub fn advance(
+        &mut self,
+        epsilon: <K as AbsDiffEq>::Epsilon,
+        portal: [([K; 2], D); 2],
+    ) -> Option<(([K; 2], D), &([K; 2], D))> {
         let apex = &mut self.apex;
-        /*
-        let mut update_vertex = |ahs: &mut ([K; 2], D),
-                                 bhs: &mut ([K; 2], D),
-                                 bportal: &([K; 2], D),
-                                 flip: bool|
-         -> Option<(([K; 2], D), &([K; 2], D))> */
         macro_rules! update_vertex {
             ($ahs:expr, $bhs:expr, $bportal:expr, $flip:expr) => {{
                 let comp = |v: K| {
                     if $flip { v > K::zero() } else { v < K::zero() }
                 };
                 if !comp(triarea2(&apex.0, &$bhs.0, &$bportal.0)) {
-                    if apex.0.abs_diff_eq(&$bhs.0, self.epsilon.clone())
+                    if apex.0.abs_diff_eq(&$bhs.0, epsilon.clone())
                         || comp(triarea2(&apex.0, &$ahs.0, &$bportal.0))
                     {
                         // Tighten the funnel
@@ -126,7 +62,9 @@ where
                         // B over A, insert A to path and restart scan from portal A point
                         let old_apex = mem::replace(apex, (*$ahs).clone());
                         *$bhs = apex.clone();
-                        return Some((old_apex, apex));
+                        if old_apex.0 != apex.0 {
+                            return Some((old_apex, apex));
+                        }
                     }
                 }
                 None
@@ -153,13 +91,13 @@ mod tests {
 
     #[test]
     fn t_simple() {
-        let mut x = SimpleFunnel::<i32, u8>::new(([0, 0], 0), 0);
-        assert_eq!(x.advance([([30, 0], 1), ([10, 40], 2)]), None);
-        assert_eq!(x.advance([([60, 20], 3), ([60, 40], 4)]), None);
+        let mut x = SimpleFunnel::<i32, u8>::new(([0, 0], 0));
+        assert_eq!(x.advance(0, [([30, 0], 1), ([10, 40], 2)]), None);
+        assert_eq!(x.advance(0, [([60, 20], 3), ([60, 40], 4)]), None);
         assert_eq!(
-            x.advance([([70, -10], 5), ([90, 20], 6)]),
+            x.advance(0, [([70, -10], 5), ([90, 20], 6)]),
             Some((([0, 0], 0), &([60, 20], 3)))
         );
-        assert_eq!(x.advance([([80, -10], 7), ([80, -10], 8)]), None);
+        assert_eq!(x.advance(0, [([80, -10], 7), ([80, -10], 8)]), None);
     }
 }
